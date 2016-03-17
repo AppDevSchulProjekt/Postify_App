bdc95d3339c18930e1d3bbbe02fa2dca418ee71d
Daniel Schmidt <daniel.schicksal@googlemail.com>
2016-03-17 14:26:46 +0100

    Informationen hinzugefügt

--- app/src/main/java/com/appdev/postify/MainActivity.java
+++ app/src/main/java/com/appdev/postify/MainActivity.java
@@ -18,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    // HttpURLConnection - http://developer.android.com/reference/java/net/HttpURLConnection.html
+    // for POST and GET Requests
+
+    // AsyncTask -  http://developer.android.com/reference/android/os/AsyncTask.html
+    // Load Data a synchron
+
+    // SQLiteOpenHelper - http://developer.android.com/reference/android/database/sqlite/SQLiteOpenHelper.html
+    // http://www.programmierenlernenhq.de/android-sqlite-datenbank-tutorial/
+
+    // Shared Preferences - http://developer.android.com/reference/android/content/SharedPreferences.html
+    // Save Values (firststart)
+
+    // Listview - http://developer.android.com/guide/topics/ui/layout/listview.html
+    // Show Table Data
+
     // @Override Methods... onCreateOptionsMenu
     // @Override Methods... onOptionsItemSelected
 }
