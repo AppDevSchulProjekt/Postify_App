db4ddab1302aabcc61b33496fd0b51e079c9d86c
Daniel Schmidt <d.schmidt@appfarms.com>
2016-04-14 11:31:36 +0200

    Add PushConfig / PushManager / PushEnvironment

---
+++ app/src/main/java/com/appdev/postify/BaseApplication.java
@@ -0,0 +1,56 @@
+package com.appdev.postify;
+
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.provider.Settings;
+import android.provider.Settings.Secure;
+
+import com.appdev.postify.activities.MainActivity;
+import com.appfarms.android.afpushmanager.AFPushConfig;
+import com.appfarms.android.afpushmanager.AFPushManager;
+import com.appfarms.android.afpushmanager.data.AFPushNotification;
+import com.appfarms.android.afpushmanager.enums.AFPushEnvironment;
+
+import java.util.HashMap;
+
+/**
+ * Created by daniel on 13.04.16.
+ */
+public class BaseApplication extends Application {
+    private final String API_KEY = "ab1fe97c2579cb11304e4d265665398b";
+    private final String SENDER_ID = "895602186875";
+    private final String ATTRIUTE_KEY_DEVICE_ID = "DEVICE_ID";
+    private String deviceID;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Get a 64-bit number as a hex string (randomly generated when the user first sets up)
+        deviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
+        // AFPushEnvironment set Live Mode
+        AFPushEnvironment environment = AFPushEnvironment.LIVE;
+        // Create AppFarms Push Config
+        AFPushConfig config = new AFPushConfig(this, API_KEY, SENDER_ID, environment) {
+            @Override
+            public PendingIntent getNotificationPendingIntent(AFPushNotification notification) {
+                // Create an intent for specific components
+                Intent intent = new Intent(BaseApplication.this, MainActivity.class);
+                // launching a new instance of that activity
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                // A description of an Intent and target action to perform with it
+                PendingIntent pendingIntent = PendingIntent.getActivity(BaseApplication.this, 0 , intent, PendingIntent.FLAG_ONE_SHOT);
+                return pendingIntent;
+            }
+        };
+        // Create AFPushManager and Register Activity Lifecycle Callbacks
+        AFPushManager.getInstance().init(config);
+        // A Map is a data structure consisting of a set of keys and values in which each key is mapped to a single value.
+        HashMap<String,Object> deviceIDAttribute = new HashMap<String,Object>();
+        // Maps the specified key to the specified value.
+        deviceIDAttribute.put(ATTRIUTE_KEY_DEVICE_ID, deviceID);
+        // Create AFPushManager and put Device Attributes
+        AFPushManager.getInstance().putDeviceAttributes(deviceIDAttribute);
+    }
+
+}
